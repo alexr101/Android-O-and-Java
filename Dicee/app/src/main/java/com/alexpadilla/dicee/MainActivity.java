73e12393215a71cb5b61af25758773b4d134a630
alexr101 <alex_r_101@hotmail.com>
2017-11-18 13:04:29 -0500

    link image view objects to mainActivity

--- Dicee/app/src/main/java/com/alexpadilla/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/alexpadilla/dicee/MainActivity.java
@@ -2,6 +2,8 @@ package com.alexpadilla.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.rollBtn);
+        ImageView dice1 = (ImageView) findViewById(R.id.dice_left);
+        ImageView dice2 = (ImageView) findViewById(R.id.dice_right);
+
     }
 }
