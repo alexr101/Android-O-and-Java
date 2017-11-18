75671b6c9e13fb21746638c2eb2cacfa880978d4
alexr101 <alex_r_101@hotmail.com>
2017-11-18 13:11:56 -0500

    add array of r.drawable image views, and a random num generator

--- Dicee/app/src/main/java/com/alexpadilla/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/alexpadilla/dicee/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,10 +20,25 @@ public class MainActivity extends AppCompatActivity {
         ImageView dice1 = (ImageView) findViewById(R.id.dice_left);
         ImageView dice2 = (ImageView) findViewById(R.id.dice_right);
 
+        int[] diceArr = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
         rollBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Dicee", "The button was pressed")
+                Log.d("Dicee", "The button was pressed");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+
             }
         });
 
