eb2e45d64ab3863d36b46f60b58540cd50ade586
alexr101 <alex_r_101@hotmail.com>
2017-11-19 01:16:43 -0500

    create question class and add an array to hold xml string data

---
+++ Quizzler-Android/app/src/main/java/com/londonappbrewery/quizzler/Question.java
@@ -0,0 +1,16 @@
+package com.londonappbrewery.quizzler;
+
+/**
+ * Created by alexpadillajr on 11/19/17.
+ */
+
+public class Question {
+
+    private int id;
+    private Boolean answer;
+
+    public Question(int id, Boolean answer) {
+        this.id = id;
+        this.answer = answer;
+    }
+}
