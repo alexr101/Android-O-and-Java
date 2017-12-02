171884f66d1e8fd1567e1d7bc864281cdf360cdc
alexr101 <alex_r_101@hotmail.com>
2017-12-02 12:12:20 -0500

    add btn, second view, and click listener to create new intnet to second view

--- Intent Tutorial/app/src/main/java/com/alexpadilla/intentapp/MainActivity.java
+++ Intent Tutorial/app/src/main/java/com/alexpadilla/intentapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.alexpadilla.intentapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button intentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        intentBtn = (Button) findViewById(R.id.intentBtn);
+
+        intentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // noise
+                Intent i = new Intent(MainActivity.this, SecondViewActivity.class);
+                startActivity(i);
+            }
+        });
+
 
         // links to layout/actvity_main.xml
         setContentView(R.layout.activity_main);
