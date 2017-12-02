d6600e63da78c6b34c95595284396fc1f76d818a
alexr101 <alex_r_101@hotmail.com>
2017-12-02 11:57:02 -0500

    Create Android Intent App, comment ways the views link

---
+++ Intent Tutorial/app/src/main/java/com/alexpadilla/intentapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alexpadilla.intentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // links to layout/actvity_main.xml
+        setContentView(R.layout.activity_main);
+    }
+}
--- RetrofitTutorial/app/src/main/java/com/alexpadilla/retrofittutorial/activity/MainActivity.java
+++ RetrofitTutorial/app/src/main/java/com/alexpadilla/retrofittutorial/activity/MainActivity.java
@@ -3,13 +3,21 @@ package com.alexpadilla.retrofittutorial.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import com.alexpadilla.retrofittutorial.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String API_KEY = "193d573c5e8bd0513c33d0ef959d57fa";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 }
